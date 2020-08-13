ac7423f727d696837acb6fc07c1d579228a866ce
Jan Klimo <jan.klimo@gmail.com>
2020-08-13 20:15:38 +0700

    More copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,13 +8,18 @@ export default function Home() {
         <title>Shopify Makers Club</title>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="An exclusive community of professionals building and growing apps on the Shopify platform."
+        />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <section className={`${styles.hero} pt-32`}>
         <div className="pl-40">
           <h1 className="text-6xl font-bold text-gray-700 mb-6">Shopify Makers Club</h1>
-          <p className="font-light text-2xl text-gray-600 mb-20">
-            An exclusive community of professionals building on Shopify's platform.
+          <p className="font-light text-2xl text-gray-600 mb-20 w-1/3">
+            An exclusive community of professionals building and growing apps on the Shopify
+            platform.
           </p>
           <a
             href="#"
